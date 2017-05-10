a1dc1613acc78408620f13595567c97986cb97f0
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2017-05-10 12:46:14 +0800

    refs 0

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <unistd.h>
+#include "test.h"
 
 int main(void)
 {
-	std::cout << " Hello World! " << std::endl;
-
+	int a = -3, b = 5;
+	
+	std::cout << " Print Result! " << std::endl;
+	std::cout << add(a, b) << std::endl;
+	
+	sleep(10);
+	
 	return 0;
 }
