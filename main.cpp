2f5b26ec89974bc79840cdd343f7655e90577ccf
U-CNYKGW\30032183 <30032183@CPC439-06288-01.cn.ykgw.net>
2017-08-14 10:25:51 +0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,69 +1,77 @@
 #include <iostream>
 #include "Common.h"
-#include "basicDerived.h"
-#include "BindingTest.h"
-#include "VirtualTest.h"
-
+#include <sstream>
+#include <string>
 #include <list>
 #include <algorithm>
 
-    const float*        m_range_table;          // range table
-    unsigned long       m_range_table_size;     // range table size
-    unsigned long       m_range;
-void setRangeList(unsigned long size, const float* list)
-{
-    if(size > 0)
-    {
-        m_range_table = list;
-        m_range_table_size = size;
-    }
-}
-float getRangeValue()
-{
-    float lvl;
-    if(m_range < m_range_table_size)
-    {
-        lvl = m_range_table[m_range];
-    }else
-    {
-        lvl = m_range_table[0];
-    }
+#define MAX_PROMPT_CHAR             (255)
 
-    return lvl;    
+template <typename T>
+inline bool FromString(const std::string& sString, T& tX)
+{
+    std::istringstream iStream(sString);
+    return (iStream >> tX) ? true : false;
 }
 
-int main()
+template <typename T>
+inline std::string ToString(T tX)
 {
-    using namespace std;
+    std::ostringstream oStream;
+    oStream << tX;
+    return oStream.str();
+}
+void SCMemCopyUnshort(void* to, const void* from,
+        unsigned short size)
+{
+    unsigned short length = 0;
+    unsigned int dst = (unsigned int)(to);
+    unsigned int src = (unsigned int)(from);
 
-    // list<int> li;
-    // for (int nCount = 0; nCount < 6; nCount++)
-    //     li.push_back(nCount);
+    if((dst == 0) || (src == 0))
+    {
+        return;
+    }
 
-    // list<int>::const_iterator it;   // declare an iterator
-    // it = min_element(li.begin(), li.end());
-    //     cout << *it << " ";
-    // it = max_element(li.begin(), li.end());
-    // cout << *it << "\n";
+    while(*(*src))
+    {
+        ptr++;
+        length++;
+    }
 
-    // list<int> li;
-    // for (int i = 0; i < 6; i++)
-    //     li.push_back(i);
-
-    // list<int>::iterator it; //declare an iterator
-    // it = find(li.begin(), li.end(), 3); // find the value 3 in the list
-    // li.insert(it, 8); // use list::insert to insert the value 8 before it
-
-    // for (it  = li.begin(); it != li.end(); it++) // for loop with iterators
-    //     cout << *it << " ";
-
-    // cout << endl;
-    static const float default_range_table[] = { 6.0,4.0,2.0 };
-    unsigned long i = sizeof(default_range_table) / sizeof(float);
-    setRangeList(i , default_range_table);
-    float f = getRangeValue();
+    if(size >= MAX_PROMPT_CHAR)
+    {
+        size = MAX_PROMPT_CHAR;
+    }
     
-    cout << i << endl;    
+    if(length < size)
+    {
+        size = length;
+    }
+
+    memcpy(to, from, size * sizeof(*to));
+}
+using namespace std;
+int main()
+{ 
+    unsigned short array[MAX_PROMPT_CHAR+1] = {1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0};
+    unsigned short array_a[MAX_PROMPT_CHAR+1] = {0};
+    //unsigned short array[10] = {0};
+     
+    for(int i = 0; i < 20; i++)
+    {
+        printf("array[%d] = %d; array_a[%d] = %d;\n", i, array[i], i, array_a[i]);
+    }
+      
+    //memcpy(array+3, array_a, sizeof(array_a));
+    //memcpy(array+3, 0x0000, 14);
+    SCMemCopyUnshort(array_a, array+1, 10);
+
+    for(int i = 0; i < 20; i++)
+    {
+        printf("array[%d] = %d; array_a[%d] = %d;\n", i, array[i], i, array_a[i]);
+    }
+
     SystemPause();
     return 0;
 }
