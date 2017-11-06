3d78f279cb247c17cfc21070cc08e065ce8ef5eb
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2017-11-06 15:33:01 +0800

    object representation and value representation

--- main.cpp
+++ main.cpp
@@ -1,141 +1,48 @@
+#include <cassert>
 #include <iostream>
-#if 0
-class Base
-{
-public:
-    Base() { Subtle(); }       // 打印 1
+#include "Common.h"
 
-    virtual void Foo() = 0;
-    void Subtle() { Foo(); }
-};
-
-class Derive : public Base
-{
-public:
-    Derive() : Base(), m_pData(new int(2)) {}
-    ~Derive() { delete m_pData; }
-
-    virtual void Foo()
-    {
-        std::cout << *m_pData << std::endl;
+struct S {
+    char c;   // 1 byte value
+             // 3 bytes padding
+    float f;   // 4 bytes value
+    bool operator==(const S& arg) const {    // value-based equality
+        return c == arg.c && f == arg.f;
     }
-
-private:
-    int* m_pData;
 };
+// assert(sizeof(S) == 8);
+S s1 = {'a', 3.14};
+S s2 = s1;
+//reinterpret_cast<char*>(&s1)[2] = 'b'; // change 2nd byte
+// assert(s1 == s2); // value did not change
 
 int main()
 {
-    Base* p = new Derive();
-    p->Foo();
-    delete p;
-    return 0;
+    std::cout << sizeof(S) << std::endl;
+
+    if (s1 == s2)
+    {
+        std::cout << "s1 == s2" << std::endl;
+    }
+    else{
+        std::cout << "s1 != s2" << std::endl;
+    }
+
+    reinterpret_cast<char *>(&s1)[2] = 'b';
+    
+    if (s1 == s2)
+    {
+        std::cout << "s1 == s2" << std::endl;
+    }
+    else{
+        std::cout << "s1 != s2" << std::endl;
+    }
+
+    std::cout << *(reinterpret_cast<char *>(&s1)) << std::endl;
+    std::cout << *(reinterpret_cast<char *>(&s1) + 1) << std::endl;
+    std::cout << *(reinterpret_cast<char *>(&s1) + 2) << std::endl;
+    std::cout << *(reinterpret_cast<char *>(&s1) + 3) << std::endl;
+    std::cout << *(reinterpret_cast<char *>(&s1) + 4) << std::endl;
+
+    SystemPause();
 }
-#endif
-
-#if 1
-class Base
-{
-public:
-    Base() { PrintBase(); }
-
-    void PrintBase()
-    {
-        std::cout << "Address of Base:" << this << std::endl;
-
-        // 虚函数表的地址存在对象内存空间里的头四个字节
-        int* vt = (int *)*((int *)this);
-        std::cout << "Address of Base Vtable: " << vt << std::endl;
-
-        // 通过vt来调用Foo函数，以证明vt指向的确实是虚函数表
-        std::cout << "Call Foo by vt->";
-        void (*pFoo)(Base* const) = (void (*)(Base* const))vt[0];
-        (*pFoo)(this);
-
-        std::cout << std::endl;
-    }
-
-    virtual void Foo() { std::cout << "Base" << std::endl; }
-};
-
-class Derive : public Base
-{
-public:
-    Derive() : Base() { PrintDerive(); }
-
-    void PrintDerive()
-    {
-        std::cout << "Address of Derive:" << this << std::endl;
-
-        // 虚函数表的地址存在对象内存空间里的头四个字节
-        int* vt = (int *)*((int *)this);
-        std::cout << "Address of Derived Vtable: " << vt << std::endl;
-
-        // 通过vt来调用Foo函数，以证明vt指向的确实是虚函数表
-        std::cout << "Call Foo by vt->";
-        void (*pFoo)(Base* const) = (void (*)(Base* const))vt[0];
-        (*pFoo)(this);
-
-        std::cout << std::endl;        
-    }
-
-    virtual void Foo() { std::cout << "Derive" << std::endl; }
-};
-
-int main()
-{
-    Base* p = new Derive();
-    delete p;
-    return 0;
-}
-#endif
-
-#if 0
-using namespace std;
-
-class Polygon 
-{
-protected:
-    int width, height;
-
-public:
-    void set_values(int a, int b)
-    {
-        width = a;
-        height = b;
-    }
-};
-
-class Rectangle : public Polygon
-{
-public:
-    int area()
-    {
-        return width * height;
-    }
-};
-
-class Triangle : public Polygon
-{
-public:
-    int area()
-    {
-        return width * height / 2;
-    }
-};
-
-int main()
-{
-    Rectangle rect;
-    Triangle trgl;
-
-    Polygon *ppoly1 = &rect;
-    Polygon *ppoly2 = &trgl;
-    ppoly1->set_values(4, 5);
-    ppoly2->set_values(4, 5);
-    cout << rect.area() << endl;
-    cout << trgl.area() << endl;
-
-    return 0;
-}
-#endif
