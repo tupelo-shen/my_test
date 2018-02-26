3857765f34b7f7ca9d7905b643bbee386eac419b
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2018-02-26 15:18:27 +0800

    2018-02-26

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include "Common.h"
 
+#if 0
 namespace Q {
     namespace V {    // V是Q的一个成员，它完全在Q内定义 
         // namespace Q::V { // C++17 替换上面两行
@@ -32,96 +33,29 @@ namespace Q {
         ABC = 'a'
     };
 }
-
-namespace A {
-    int i;
-}
-namespace B {
-    int i;
-    int j;
-    namespace C {
-        namespace D {
-            using namespace A;  // A 中所有的名称被导入到全局命名空间中
-            int j;
-            int k;
-            int a = i;          // i是B::i, 因为A::i被B::i覆盖
-        }
-        using namespace D;      // D 中所有的名称被导入到 C 命名空间中
-                                // A 中所有的名称被导入到全局命名空间中
-        int k = 89; // OK to declare name identical to one introduced by a using
-        int l = k;  // ambiguous: C::k or D::k
-        int m = i;  // ok: B::i hides A::i
-        int n = j;  // ok: D::j hides B::j
+#endif
+#if 1
+namespace Q {
+    namespace V { // V原始命名空间定义
+        void f(); // Q::V::f的声明
+    }
+    void V::f() {} // OK
+    //void V::g() {} // Error: g()还不是V的成员
+    namespace V { // V的扩展命名空间定义
+        void g(); // Q::V::g的声明
+    }
+    namespace R {           // 不是Q的一个封闭命名空间
+        void g() {}    // Error: cannot define Q::V::g inside R
     }
 }
-namespace D 
-{
-   int d1;
-   void f(char);
-}
-using namespace D;  // 导入D::d1, D::f, D::d2, D::f, E::e 和 E::f到全局命名空间中
- 
-int d1;             // OK: 声明时和D::d1没有冲突
-namespace E {
-    int e;
-    void f(int);
-}
 
-// 扩展命名空间
-namespace D 
-{
-    int d2;
-    using namespace E; // transitive using-directive
-    void f(int);
-}
-void f() {
-    d1++; // error: ambiguous ::d1 or D::d1?
-    ::d1++; // OK
-    D::d1++; // OK
-    d2++; // OK, d2 is D::d2
-    e++; // OK: e is E::e due to transitive using
-    f(1); // error: ambiguous: D::f(int) or E::f(int)?
-    f('a'); // OK: the only f(char) is D::f(char)
-}
-namespace X {
-    namespace M {
-        void g(); // declares, but doesn't define X::M::g()
-    }
-    using M::g;
-    void g();   // Error: attempt to declare X::g which conflicts with X::M::g()
-}
-
-
-void h(int);
-namespace A {
-    class X {
-        friend void f(X); // A::f is a friend
-        class Y {
-            friend void g(); // A::g is a friend
-            friend void h(int); // A::h is a friend, no conflict with ::h
-        };
-    };
-    // A::f, A::g and A::h are not visible at namespace scope
-    // even though they are members of the namespace A
-    X x;
-    void g() {  // definition of A::g
-        f(x); // A::X::f is found through ADL
-    }
-    void f(X) {}       // definition of A::f
-    void h(int) {}     // definition of A::h
-    // A::f, A::g and A::h are now visible at namespace scope
-    // and they are also friends of A::X and A::X::Y
-}
-
+//void Q::V::g() {} // OK: 全局命名空间包括Q
+#endif
 
 
 int main()
 {
-    char abc[10] = {Q::ABC};
-    Q::V::C c;
-    c.m();
-
-    printf("ABC = %s", abc);
+    printf("ABC = ");
 
     SystemPause();
 }
