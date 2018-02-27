76179fa973a294f63f9784225fb9b62ce4d13e93
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2018-02-27 12:54:11 +0800

    completed threadsafe queue-2018-02-27

--- main.cpp
+++ main.cpp
@@ -1,61 +1,45 @@
-#include <algorithm>
-#include <cctype>
+//#include <vector>
 #include <iostream>
-#include <string>
-#include <vector>
+#include <chrono>
+#include <thread>
 #include "Common.h"
+#include "Queue-pthread_s.h"
 
-#if 0
-namespace Q {
-    namespace V {    // V是Q的一个成员，它完全在Q内定义 
-        // namespace Q::V { // C++17 替换上面两行
-        // C是V的一个成员，它完全在V内定义;C::m只被声明；
-        class C { 
-        public:
-            void m(); 
-        };
-        // f是V的一个成员，在这里只定义
-        void f(); 
-    }
-    // V外的V成员函数f的定义，f的封闭命名空间仍然是全局命名空间Q和Q :: V
-    void V::f()
+using namespace std::chrono_literals;
+
+int g_index = 10;
+Queue_s<int> q;
+
+void threadFunction_1()
+{
+    std::thread::id this_id = std::this_thread::get_id();
+    std::cout << "thread [" << this_id << "] start...\n";
+    while(true)
     {
-        extern void h(); // This declares ::Q::V::h
-    }
-    // 命名空间和类外 V::C::m 定义，的封闭命名空间仍然是全局命名空间Q和Q::V
-    void V::C::m()
-    {
-        printf("V::C::m()");
+        //std::this_thread::sleep_for(1.5s);
+        int value = 0;
+        value = q.wait_and_pop();
+        printf("wait_and_pop done! value=%d  thread id:%d\n", value, this_id);
     }
 }
-namespace Q {
-    enum {
-        ABC = 'a'
-    };
-}
-#endif
-#if 1
-namespace Q {
-    namespace V { // V原始命名空间定义
-        void f(); // Q::V::f的声明
-    }
-    void V::f() {} // OK
-    //void V::g() {} // Error: g()还不是V的成员
-    namespace V { // V的扩展命名空间定义
-        void g(); // Q::V::g的声明
-    }
-    namespace R {           // 不是Q的一个封闭命名空间
-        void g() {}    // Error: cannot define Q::V::g inside R
+void threadFunction_2()
+{
+    std::cout << "threadFunction_2 start\n";
+    while(true)
+    {     
+        std::this_thread::sleep_for(1s);  
+        g_index++;
+        q.push(g_index);
+        printf("push %d\n", g_index); 
     }
 }
-
-//void Q::V::g() {} // OK: 全局命名空间包括Q
-#endif
-
-
 int main()
 {
-    printf("ABC = ");
+    std::thread thd1(threadFunction_2);
+    std::thread thd2(threadFunction_1);
+
+    thd1.join();
+    thd2.join();
 
     SystemPause();
 }
--- src/Queue-pthread_s.h
+++ src/Queue-pthread_s.h
@@ -1,207 +1,109 @@
+/*
+ * @Module Name：    Queue-pthread.h 
+ * @Function：
+ * @Author:         Tupelo Shen
+ * @Create Date:    2018-02-27
+ */
+#ifndef __QUEUE_PTHREAD_S_H__
+#define __QUEUE_PTHREAD_S_H__
+
 #include <iostream>
-//#include <vector>
 #include <thread>
 #include <mutex>
-#include <condition_variable>
 #include <queue>
+#include <condition_variable>
 #include <initializer_list>
 
-// 这个是线程安全的队列，Synchoronized类的实现在下面
-// 因为有std::mutex和std::condition_variable类成员,所以此类不支持复制构造函数也不支持赋值操作符(=)
-template <class Type>
+/*
+ * 线程安全队列
+ * T为队列元素类型
+ * 因为有std::mutex和std::condition_variable类成员,所以此类不支持复制构造函数也不支持赋值操作符(=)
+ */
+template <typename Type>
 class Queue_s
 {
 private:
-    std::mutex                  m_mutex;
-    std::condition_variable     m_cv;     
-    std::queue<Type>            *m_queue;
+    mutable std::mutex                  m_mutex;
+    mutable std::condition_variable     m_cv;
+    using   queue_type = std::queue<Type>;
+    queue_type                          m_queue;     
+    //std::queue<Type>            *m_queue;
 
 public:
-    Queue_s()
-    {
-        m_queue = new std::queue<Type>();
-    };
+    using value_type= typename queue_type::value_type;
+    using container_type = typename queue_type::container_type;
+    Queue_s()=default;
     Queue_s(const Queue_s&)=delete;
     Queue_s& operator=(const Queue_s&)=delete;
 
     /*
      * 使用迭代器为参数的构造函数,适用所有容器对象
-     * */
-    // template<typename _InputIterator>
-    // threadsafe_queue(_InputIterator first, _InputIterator last){
-    //     for(auto itor=first;itor!=last;++itor){
-    //         data_queue.push(*itor);
-    //     }
-    // }
-
-    ~Queue_s(){delete m_queue;};
-
-    void push(const Type& type)
+     */
+    template<typename _InputIterator>
+    Queue_s(_InputIterator first, _InputIterator last)
     {
-        std::lock_guard<std::mutex> lk(m_mutex);   
-        
-        m_queue->push(type);  
-        try  
-        {  
-            m_cv.notify_one();  
-        }  
-        catch(std::exception& ex)  
-        {  
-            std::cout << ex.what()<< std::endl;  
-            throw;  
-        } 
-    };
-    Type pop_nonblock()
-    {
-        Type type;  
-   
-        m_mutex.lock();
-        while(!m_queue->empty())
-        {  
-            type = m_queue->front();  
-            m_queue->pop();                 
+        for(auto itor=first;itor!=last;++itor){
+            m_queue.push(*itor);
         }
-        m_mutex.unlock();
+    }
+    explicit Queue_s(const container_type &c):m_queue(c){}
+    /*
+     * 使用初始化列表为参数的构造函数
+     */
+    Queue_s(std::initializer_list<value_type> list):Queue_s(list.begin(),list.end()){}
 
-        return type;
-    };
-    Type pop_block()
+    /*
+     * 将元素加入队列
+     */
+    void push(const value_type &new_value)
     {
-        Type type;  
+        std::lock_guard<std::mutex> lk(m_mutex);
+        m_queue.push(std::move(new_value));
+        m_cv.notify_one();
+    }
 
-        m_mutex.lock();
-        // while(m_queue->empty())
-        // {  
-        //     try  
-        //     {  
-        //         wait();
-        //     }
-        //     catch(exception& ex)
-        //     {
-        //         cout<< ex.what()<<endl;  
-        //         throw;  
-        //     }                  
-        // }  
+    /*
+     * 从队列中弹出一个元素,如果队列为空就阻塞
+     */
+    value_type wait_and_pop()
+    {
+        std::unique_lock<std::mutex> lk(m_mutex);
+        m_cv.wait(lk, [this]{return !this->m_queue.empty();});
+        auto value=std::move(m_queue.front());
+        m_queue.pop();
+        return value;
+    }
 
-        type = m_queue->front();  
-        m_queue->pop();
-        m_mutex.unlock();
+    /*
+     * 从队列中弹出一个元素,如果队列为空返回false
+     */
+    bool try_pop(value_type& value)
+    {
+        std::lock_guard<std::mutex> lk(m_mutex);
+        if(m_queue.empty())
+            return false;
+        value=std::move(m_queue.front());
+        m_queue.pop();
+        return true;
+    }
 
-        return type; 
-    };
-};
+    /*
+     * 返回队列是否为空
+     */
+    auto empty() const->decltype(m_queue.empty()) 
+    {
+        std::lock_guard<std::mutex> lk(m_mutex);
+        return m_queue.empty();
+    }
 
-/*
- * @func:   Queue_s()
- * @Desc:   构造函数
- */
-// template <class Type>
-// Queue_s<Type>::Queue_s()
-// {
-//     m_queue = new std::queue<Type>();
-// }
+    /*
+     * 返回队列中元素数个
+     */
+    auto size() const->decltype(m_queue.size())
+    {
+        std::lock_guard<std::mutex> lk(m_mutex);
+        return m_queue.size();
+    }
+}; /* Queue_s */
 
-/*
- * @func:   ~Queue_s()
- * @Desc:   析构函数
- */
-// template <class Type>
-// Queue_s<Type>::~Queue_s()
-// {
-//     delete m_queue;
-// }
-
-/*
- * @func:   pop()
- * @Desc:   从队列中弹出一项
- */
-// template <class Type>     
-// Type Queue_s<Type>::pop_nonblock()
-// {  
-//     Type type;  
-   
-//     m_mutex.lock();
-//     while(!m_queue->empty())
-//     {  
-//         type = m_queue->front();  
-//         m_queue->pop();                 
-//     }
-//     m_mutex.unlock();
-
-//     return type; 
-// }
-
-// /*
-//  * @func:   pop()
-//  * @Desc:   从队列中弹出一项
-//  */
-// template <class Type>     
-// Type Queue_s<Type>::pop_block()
-// {  
-//     Type type;  
-   
-//     m_mutex.lock();
-//     // while(m_queue->empty())
-//     // {  
-//     //     try  
-//     //     {  
-//     //         wait();
-//     //     }
-//     //     catch(exception& ex)
-//     //     {
-//     //         cout<< ex.what()<<endl;  
-//     //         throw;  
-//     //     }                  
-//     // }  
-
-//     type = m_queue->front();  
-//     m_queue->pop();
-//     m_mutex.unlock();
-
-//     return type; 
-// }
-
-/*
- * @func:   push()
- * @Desc:   压入队列
- */
-// template <class Type>    
-// void Queue_s<Type>::push(const Type &type)
-// {  
-//     std::lock_guard<std::mutex> lk(m_mutex);   
-    
-//     m_queue->push(type);  
-//     try  
-//     {  
-//         m_cv.notify_one();  
-//     }  
-//     catch(std::exception& ex)  
-//     {  
-//         std::cout << ex.what()<< std::endl;  
-//         throw;  
-//     }   
-// }  
-
-int f(Queue_s<int>& queue)
-{
-    static int i = 0;
-    //synchronized { // begin synchronized block
-        std::cout << i << " -> ";
-        ++i;       // each call to f() obtains a unique value of i
-        queue.push(i);
-        std::cout << i << '\n';
-        return i; // end synchronized block
-    //}
-}
-#include <vector>
-int testQueue_s()
-{
-    Queue_s<int> q();
-    //Queue_s<int> &q_tmp = q;
-    std::vector<std::thread> v(10);
-    for(auto& t: v)
-        t = std::thread([]{ for(int n = 0; n < 10; ++n) f(q); });
-    for(auto& t: v)
-        t.join();
-    return 0;
-}
+#endif /* __QUEUE_PTHREAD_S_H__ */
