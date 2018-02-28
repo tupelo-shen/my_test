c41a6a423f2d905afe52584d1e46fcc347e84152
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2018-02-28 18:44:47 +0800

    object-test-2018/02/28

--- main.cpp
+++ main.cpp
@@ -1,45 +1,24 @@
-//#include <vector>
+#include <cassert>
 #include <iostream>
-#include <chrono>
-#include <thread>
 #include "Common.h"
-#include "Queue-pthread_s.h"
 
-using namespace std::chrono_literals;
-
-int g_index = 10;
-Queue_s<int> q;
-
-void threadFunction_1()
-{
-    std::thread::id this_id = std::this_thread::get_id();
-    std::cout << "thread [" << this_id << "] start...\n";
-    while(true)
-    {
-        //std::this_thread::sleep_for(1.5s);
-        int value = 0;
-        value = q.wait_and_pop();
-        printf("wait_and_pop done! value=%d  thread id:%d\n", value, this_id);
+struct S {
+    char c;            
+    float f;   
+    bool operator==(const S& arg) const {   
+        return c == arg.c && f == arg.f;
     }
-}
-void threadFunction_2()
-{
-    std::cout << "threadFunction_2 start\n";
-    while(true)
-    {     
-        std::this_thread::sleep_for(1s);  
-        g_index++;
-        q.push(g_index);
-        printf("push %d\n", g_index); 
-    }
-}
-int main()
-{
-    std::thread thd1(threadFunction_2);
-    std::thread thd2(threadFunction_1);
+};
 
-    thd1.join();
-    thd2.join();
+
+int main(void)
+{
+    assert(sizeof(S) == 8);
+    S s1 = {'a', 3.14};
+    S s2 = s1;
+    reinterpret_cast<char*>(&s1)[2] = 'b';
+    printf("reinterpret_cast<char*>(&s1)[2] = %c\n", reinterpret_cast<char*>(&s1)[2]); 
+    assert(s1 == s2);
 
     SystemPause();
 }
