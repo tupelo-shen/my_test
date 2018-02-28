a03aaf9cf836f80b5873378e98864241e262b38f
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2018-02-28 18:44:47 +0800

    object-test-2018/02/28

--- main.cpp
+++ main.cpp
@@ -1,56 +1,24 @@
-//#include <vector>
+#include <cassert>
 #include <iostream>
-#include <chrono>
-#include <thread>
 #include "Common.h"
-#include "Queue-pthread_s.h"
 
-using namespace std::chrono_literals;
-
-int g_index = 10;
-Queue_s<int> q;
-std::initializer_list<float> list = {1, 2, 3, 4, 5};
-Queue_s<float> q1(list);
-
-void threadFunction_1()
-{
-    std::this_thread::sleep_for(1s);
-    std::thread::id this_id_1 = std::this_thread::get_id();
-    // std::cout << "thread[" << this_id_1 << "] start..." << std::endl;
-    while(true)
-    {
-        std::this_thread::sleep_for(0.5s);
-        int value = 0;
-        // value = q.wait_and_pop();
-        if (q.try_pop((int&)value))
-        {
-            std::cout << "thread[" << this_id_1 <<
-                "] poped: " << value << std::endl;
-        }
+struct S {
+    char c;            
+    float f;   
+    bool operator==(const S& arg) const {   
+        return c == arg.c && f == arg.f;
     }
-}
-void threadFunction_2()
-{
-    std::this_thread::sleep_for(0.5s);
-    std::thread::id this_id_2 = std::this_thread::get_id();
-    std::cout << "thread[" << this_id_2 << "] start..." << std::endl;
-    while(true)
-    {     
-        std::this_thread::sleep_for(1s);  
-        g_index++;
-        q.push(g_index);
-        std::cout << "thread[" << this_id_2 << "] pushed:" << g_index << std::endl; 
-    }
-}
-int main()
-{
-    std::thread thd1(threadFunction_2);
-    std::thread thd2(threadFunction_1);
-    std::thread thd3(threadFunction_1);
+};
 
-    thd1.join();
-    thd2.join();
-    thd3.join();
+
+int main(void)
+{
+    assert(sizeof(S) == 8);
+    S s1 = {'a', 3.14};
+    S s2 = s1;
+    reinterpret_cast<char*>(&s1)[2] = 'b';
+    printf("reinterpret_cast<char*>(&s1)[2] = %c\n", reinterpret_cast<char*>(&s1)[2]); 
+    assert(s1 == s2);
 
     SystemPause();
 }
