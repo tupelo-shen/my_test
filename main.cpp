f2a4d2354318110eeabc49781eba3ff260c6d4fa
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2018-03-07 10:57:15 +0800

    2018/03/07

--- main.cpp
+++ main.cpp
@@ -2,23 +2,28 @@
 #include <iostream>
 #include "Common.h"
 
-struct S {
-    char c;            
-    float f;   
-    bool operator==(const S& arg) const {   
-        return c == arg.c && f == arg.f;
-    }
+
+//#include <cassert>
+struct Base {};             // 空类
+struct Derived1 : Base {
+    int i;
+};
+struct Derived2 : Base {
+    Base c;                // Base, 占据1个字节, 剩余字节填充
+    int i;
+};
+struct Derived3 : Base {
+    Derived1 c;           // 派生于Base, 占据sizeof(int)个字节
+    int i;
 };
 
-
-int main(void)
+int main()
 {
-    assert(sizeof(S) == 8);
-    S s1 = {'a', 3.14};
-    S s2 = s1;
-    reinterpret_cast<char*>(&s1)[2] = 'b';
-    printf("reinterpret_cast<char*>(&s1)[2] = %c\n", reinterpret_cast<char*>(&s1)[2]); 
-    assert(s1 == s2);
+    // 空基优化没有应用，base占用1个字节，Base成员占据1个字节，随后2个字节的填充，以满足对齐要求
+    assert(sizeof(Derived2) == 2*sizeof(int));
+ 
+    // 空基优化没有应用,base成员至少占用1个字节，然后加上填充物以满足第一个成员的对齐要求
+    assert(sizeof(Derived3) == 3*sizeof(int));
 
     SystemPause();
 }
