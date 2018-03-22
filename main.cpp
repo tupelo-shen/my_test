2ca8322aad48e8432d9dd94dda96ceeb7c56c259
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2018-03-22 16:05:08 +0800

    2018/03/22

--- main.cpp
+++ main.cpp
@@ -1,29 +1,16 @@
-#include <cassert>
-#include <iostream>
+// 移动构造函数和赋值
+//#include <iostream>
+#include <string>
 #include "Common.h"
 
-
-//#include <cassert>
-struct Base {};             // 空类
-struct Derived1 : Base {
-    int i;
+#include <iostream>
+struct S {
+    unsigned char b1 : 3;
+    unsigned char :0; // 启动一个新的字节
+    unsigned char b2 : 3;
+    unsigned char b3 : 1;
 };
-struct Derived2 : Base {
-    Base c;                // Base, 占据1个字节, 剩余字节填充
-    int i;
-};
-struct Derived3 : Base {
-    Derived1 c;           // 派生于Base, 占据sizeof(int)个字节
-    int i;
-};
-
 int main()
 {
-    // 空基优化没有应用，base占用1个字节，Base成员占据1个字节，随后2个字节的填充，以满足对齐要求
-    assert(sizeof(Derived2) == 2*sizeof(int));
- 
-    // 空基优化没有应用,base成员至少占用1个字节，然后加上填充物以满足第一个成员的对齐要求
-    assert(sizeof(Derived3) == 3*sizeof(int));
-
-    SystemPause();
+    std::cout << sizeof(S) << '\n'; // 通常会打印2
 }
