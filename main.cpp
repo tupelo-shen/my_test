079b0074f6d8a0e53e56f72717bc86e795761ee0
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2018-04-18 13:46:37 +0800

    2018/04/18

--- main.cpp
+++ main.cpp
@@ -1,29 +1,17 @@
 #include <iostream>
- 
-extern "C" int func();
-// 函数func的定义使用汇编语言编写
-// 原始字符串文字可能非常有用
-asm(R"(
-.globl func
-    .type func, @function
-    func:
-    .cfi_startproc
-    movl $7, %eax
-    ret
-    .cfi_endproc
-)");
- 
-int main()
+#include <math.h>
+
+float a = 0.999f/1.0e-9f;
+float b = 9.99e+8;
+
+int main(void)
 {
-    int n = func();
-    // 扩展内联汇编
-    asm ("leal (%0,%0,4),%0"
-         : "=r" (n)
-         : "0" (n));
-    std::cout << "7*5 = " << n << std::endl; // 冲刷
- 
-    // 标准内联汇编
-    asm ("movq $60, %rax\n\t" // Linux上的退出系统调用数字
-         "movq $2,  %rdi\n\t" // 这个程序返回 2
-         "syscall");
+    printf("a = %f\n b = %f\n", a, b);
+
+    float err1 = fabsf(a-b);
+    float err2 = fabsf((a-b)/b);
+
+    printf("err1 = %f\nerr2 = %f\n", err1, err2);
+
+    return 0;   
 }
