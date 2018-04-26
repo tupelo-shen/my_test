0da254de360a9179137d80c7babc16f2fb935d8c
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2018-04-26 18:26:03 +0800

    2018/04/26

--- main.cpp
+++ main.cpp
@@ -1,29 +1,83 @@
-#include <iostream>
-#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Common.h"
 
-float a = 0.999f/1.0e-9f;
-float b = 9.99e+8;
-
-int main(void)
+int main()
 {
-    printf("a = %f\n b = %f\n", a, b);
-
-    float err1 = fabsf(a-b);
-    float err2 = fabsf((a-b)/b);
-
-    printf("err1 = %f\nerr2 = %f\n", err1, err2);
-
-    char test[10]={'a','b','c','d',0};
-    for(int i=0; i<10; i++)
+    //const char *s = "0123456789012345678901234567890";
+    char array[10] = {'0','1','2','3','4','5','6','7','8','9'};
+    char *p;
+    p = strchr(array, '9');
+    printf("%ld\n", array);
+    printf("%ld\n", p);
+    printf("%d\n",*(p+1));
+    SystemPause();
+    return 0;
+}
+///**
+// * @brief   getDpFromFormat関数
+// *
+// *  小数桁数の位置を取得する
+// *
+// * @param[in]   なし
+// * @return      dp         小数桁数の位置
+// */
+//unsigned short dlgKeyboard::getDpFromFormat(void)
+//{
+//    unsigned short dp = 0;
+//
+//    for(int i=0; (m_info.format[i]!=0) && (i<(FORMAT_STR_SIZE-1));i++)
+//    {
+//        if(m_info.format[i] == '.')
+//        {
+//            dp = i+1;
+//        }
+//    }
+//
+//    return dp;
+//}
+#include <iostream>
+ 
+// simple non-type template parameter
+template<int N>
+struct S { int a[N]; };
+ 
+template<const char*>
+struct S2 {};
+ 
+// complicated non-type example
+template
+<
+    char c, // integral type
+    int (&ra)[5], // lvalue reference to object (of array type)
+    int (*pf)(int), // pointer to function
+    int (S<10>::*a)[10] // pointer to member object (of type int[10])
+> struct Complicated
+{
+    // calls the function selected at compile time
+    // and stores the result in the array selected at compile time
+    void foo(char base)
     {
-        printf("char i = %c\n", test[i]);
+        ra[4] = pf(c - base);
     }
-
-    sprintf(test, "%d", 5);
-    for(int i=0; i<10; i++)
-    {
-        printf("char i = %c\n", test[i]);
-    }
-
-    return 0;   
+};
+ 
+S2<"fail"> s2; // error: string literal cannot be used
+char okay[] = "okay"; // static object with linkage
+S2< &okay[0] > s2; // error: array element has no linkage
+S2<okay> s2; // works
+ 
+int a[5];
+int f(int n) { return n; }
+ 
+int main()
+{
+    S<10> s; // s.a is an array of 10 int
+    s.a[9] = 4;
+ 
+    Complicated<'2', a, f, &S<10>::a> c;
+    c.foo('0');
+ 
+    std::cout << s.a[9] << a[4] << '\n';
 }
