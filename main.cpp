9f2647cec63d42fb0aacd12241b909fe35ca2320
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2018-05-03 18:11:07 +0800

    2018/05/03

--- main.cpp
+++ main.cpp
@@ -1,83 +1,84 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "Common.h"
-
-int main()
-{
-    //const char *s = "0123456789012345678901234567890";
-    char array[10] = {'0','1','2','3','4','5','6','7','8','9'};
-    char *p;
-    p = strchr(array, '9');
-    printf("%ld\n", array);
-    printf("%ld\n", p);
-    printf("%d\n",*(p+1));
-    SystemPause();
-    return 0;
-}
-///**
-// * @brief   getDpFromFormat関数
-// *
-// *  小数桁数の位置を取得する
-// *
-// * @param[in]   なし
-// * @return      dp         小数桁数の位置
-// */
-//unsigned short dlgKeyboard::getDpFromFormat(void)
-//{
-//    unsigned short dp = 0;
-//
-//    for(int i=0; (m_info.format[i]!=0) && (i<(FORMAT_STR_SIZE-1));i++)
-//    {
-//        if(m_info.format[i] == '.')
-//        {
-//            dp = i+1;
-//        }
-//    }
-//
-//    return dp;
-//}
+#if 0
 #include <iostream>
  
-// simple non-type template parameter
-template<int N>
-struct S { int a[N]; };
+struct A { double x; };
+const A* a;
  
-template<const char*>
-struct S2 {};
+decltype(a->x) y;                           // y的类型是double
+decltype((a->x)) z = y;                     // z的类型是const double& (左值表达式)
  
-// complicated non-type example
-template
-<
-    char c, // integral type
-    int (&ra)[5], // lvalue reference to object (of array type)
-    int (*pf)(int), // pointer to function
-    int (S<10>::*a)[10] // pointer to member object (of type int[10])
-> struct Complicated
+template<typename T, typename U>
+auto add(T t, U u) -> decltype(t + u)       // 返回类型依赖模板参数//返回类型可以被推导（C++14之后）
 {
-    // calls the function selected at compile time
-    // and stores the result in the array selected at compile time
-    void foo(char base)
-    {
-        ra[4] = pf(c - base);
-    }
-};
- 
-S2<"fail"> s2; // error: string literal cannot be used
-char okay[] = "okay"; // static object with linkage
-S2< &okay[0] > s2; // error: array element has no linkage
-S2<okay> s2; // works
- 
-int a[5];
-int f(int n) { return n; }
- 
-int main()
-{
-    S<10> s; // s.a is an array of 10 int
-    s.a[9] = 4;
- 
-    Complicated<'2', a, f, &S<10>::a> c;
-    c.foo('0');
- 
-    std::cout << s.a[9] << a[4] << '\n';
+    return t+u;
 }
+ 
+int main() 
+{
+    int i = 33;
+    decltype(i) j = i * 2;
+ 
+    std::cout << "i = " << i << ", " << "j = " << j << '\n';
+ 
+    auto f = [](int a, int b) -> int
+    {
+        return a * b;
+    };
+ 
+    decltype(f) g = f;                      // lambda函数的类型是唯一且未命名的
+    i = f(2, 2);
+    j = g(3, 3);
+ 
+    std::cout << "i = " << i << ", " << "j = " << j << '\n';
+}
+#endif
+
+#if 1
+#include <iostream>
+#include <string>
+#include <typeinfo>
+ 
+struct Base {};                             // 非多态
+struct Derived : Base {};
+ 
+struct Base2 { virtual void foo() {} };     // 多态
+struct Derived2 : Base2 {};
+ 
+int main() {
+    int                 myint       = 50;
+    std::string         mystr       = "string";
+    double*             mydoubleptr = nullptr;
+ 
+    std::cout << "myint has type: " << typeid(myint).name() << '\n'
+              << "mystr has type: " << typeid(mystr).name() << '\n'
+              << "mydoubleptr has type: " << typeid(mydoubleptr).name() << '\n';
+ 
+    // std::cout << myint是多态类型的泛左值表达式；它会被求值
+    const std::type_info& r1 = typeid(std::cout << myint);
+    std::cout << '\n' << "std::cout<<myint has type : " << r1.name() << '\n';
+ 
+    // std::printf()不是多态类型的泛左值表达式；不会被求值
+    const std::type_info& r2 = typeid(std::printf("%d\n", myint));
+    std::cout << "printf(\"%d\\n\",myint) has type : " << r2.name() << '\n';
+ 
+    // 非多态左值是一个static类型
+    Derived d1;
+    Base& b1 = d1;
+    std::cout << "reference to non-polymorphic base: " << typeid(b1).name() << '\n';
+ 
+    Derived2 d2;
+    Base2& b2 = d2;
+    std::cout << "reference to polymorphic base: " << typeid(b2).name() << '\n';
+ 
+    try {
+        // 解引用一个null指针： 对于非多态表达式是OK的
+        std::cout << "mydoubleptr points to " << typeid(*mydoubleptr).name() << '\n'; 
+        // 解引用一个null指针： 对于多态表达式不是OK的
+        Derived2* bad_ptr = nullptr;
+        std::cout << "bad_ptr points to... ";
+        std::cout << typeid(*bad_ptr).name() << '\n';
+    } catch (const std::bad_typeid& e) {
+         std::cout << " caught " << e.what() << '\n';
+    }
+}
+#endif
