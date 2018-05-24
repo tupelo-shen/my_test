cc6074d00216d7c7776b555d4310f96e873e408b
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2018-05-24 11:41:33 +0800

    2018/05/24

--- main.cpp
+++ main.cpp
@@ -1,200 +1,146 @@
-#if 0
-#include <iostream>
- 
-struct A { double x; };
-const A* a;
- 
-decltype(a->x) y;                           // y的类型是double
-decltype((a->x)) z = y;                     // z的类型是const double& (左值表达式)
- 
-template<typename T, typename U>
-auto add(T t, U u) -> decltype(t + u)       // 返回类型依赖模板参数//返回类型可以被推导（C++14之后）
-{
-    return t+u;
-}
- 
-int main() 
-{
-    int i = 33;
-    decltype(i) j = i * 2;
- 
-    std::cout << "i = " << i << ", " << "j = " << j << '\n';
- 
-    auto f = [](int a, int b) -> int
-    {
-        return a * b;
-    };
- 
-    decltype(f) g = f;                      // lambda函数的类型是唯一且未命名的
-    i = f(2, 2);
-    j = g(3, 3);
- 
-    std::cout << "i = " << i << ", " << "j = " << j << '\n';
-}
-#endif
-
-#if 0
 #include <iostream>
 #include <string>
 #include <typeinfo>
- 
-struct Base {};                             // 非多态
-struct Derived : Base {};
- 
-struct Base2 { virtual void foo() {} };     // 多态
-struct Derived2 : Base2 {};
- 
-int main() {
-    int                 myint       = 50;
-    std::string         mystr       = "string";
-    double*             mydoubleptr = nullptr;
- 
-    std::cout << "myint has type: " << typeid(myint).name() << '\n'
-              << "mystr has type: " << typeid(mystr).name() << '\n'
-              << "mydoubleptr has type: " << typeid(mydoubleptr).name() << '\n';
- 
-    // std::cout << myint是多态类型的泛左值表达式；它会被求值
-    const std::type_info& r1 = typeid(std::cout << myint);
-    std::cout << '\n' << "std::cout<<myint has type : " << r1.name() << '\n';
- 
-    // std::printf()不是多态类型的泛左值表达式；不会被求值
-    const std::type_info& r2 = typeid(std::printf("%d\n", myint));
-    std::cout << "printf(\"%d\\n\",myint) has type : " << r2.name() << '\n';
- 
-    // 非多态左值是一个static类型
-    Derived d1;
-    Base& b1 = d1;
-    std::cout << "reference to non-polymorphic base: " << typeid(b1).name() << '\n';
- 
-    Derived2 d2;
-    Base2& b2 = d2;
-    std::cout << "reference to polymorphic base: " << typeid(b2).name() << '\n';
- 
-    try {
-        // 解引用一个null指针： 对于非多态表达式是OK的
-        std::cout << "mydoubleptr points to " << typeid(*mydoubleptr).name() << '\n'; 
-        // 解引用一个null指针： 对于多态表达式不是OK的
-        Derived2* bad_ptr = nullptr;
-        std::cout << "bad_ptr points to... ";
-        std::cout << typeid(*bad_ptr).name() << '\n';
-    } catch (const std::bad_typeid& e) {
-         std::cout << " caught " << e.what() << '\n';
-    }
-}
-#endif
-#if 1
-#include <iostream>
-#include <string.h>
+#include <float.h>
+#include <limits.h>
+#include <math.h>
 
-#define SC_MS_CODE_STR_NUM              (128)
-#define SC_MS_CODE_DELIMITER_DASH       ("-")
-#define SC_MS_CODE_DELIMITER_SLASH      ("/")
-#define SC_MODEL_FIRST_DELIMITER        (0)
-#define SC_MODEL_END_DELIMITER          (0)
-#define SC_SUFFIX1_FIRST_DELIMITER      (1)
-#define SC_SUFFIX1_END_DELIMITER        (3)
-#define SC_SUFFIX2_FIRST_DELIMITER      (4)
-#define SC_SUFFIX2_END_DELIMITER        (8)
-#define SC_SUFFIX3_FIRST_DELIMITER      (9)
-#define SC_SUFFIX3_END_DELIMITER        (11)
-#define SC_OPTIONS_FIRST_DELIMITER      (1)
-#define SC_OPTIONS_END_DELIMITER        (5)
+using namespace std;
 
-// const char* str = "FLXA402-A-B-AB-NN-NN-NA-WR-N-N-J-NN/UM/H6/SCT/CB4/K";
-// const char* str = "FLXA402-A-B-AB-NN-NN-NA-WR-N-N-J-/UM/H6/SCT/CB4/K";
-const char* str = "FLXA402-A-B-AB-NN-NN-NA-WR-/UM/H6/SCT/CB4/K";
-void spritMsCodeStr(
-    const char* str,
-    char* str_buff,
-    const char* delimiter,
-    unsigned short first_delimiter_pos,
-    unsigned short end_delimiter_pos
-    )
+// 数字入力のFloat上下限ための構造体定義
+typedef struct {
+    float               max;    // 上限値
+    float               min;    // 下限値
+}FLOAT_LIMIT_INFO;
+// 数字入力のint上下限ための構造体定義
+typedef struct {
+    int                 max;    // 上限値
+    int                 min;    // 下限値
+}INT_LIMIT_INFO;
+// 数字入力のuint上下限ための構造体定義
+typedef struct {
+    unsigned int        max;    // 上限値
+    unsigned int        min;    // 下限値
+}UINT_LIMIT_INFO;
+// 数字入力の上下限ためのunion定義
+typedef union _LIMIT_INFO
 {
-    int len, i, j;
-    char* ptr;
-    char buff[SC_MS_CODE_STR_NUM+1];
+    FLOAT_LIMIT_INFO    float_limit;
+    INT_LIMIT_INFO      int_limit;
+    UINT_LIMIT_INFO     uint_limit;
+} LIMIT_INFO;
 
-    strncpy(buff, str, SC_MS_CODE_STR_NUM);
-    buff[SC_MS_CODE_STR_NUM] = '\0';
+typedef struct _KBD_PARAM
+{
+    unsigned short      max_size;
+    LIMIT_INFO          limit;
+    _KBD_PARAM():
+    max_size(getSize()),
+    limit({FLT_MAX, -FLT_MAX}){} 
+    unsigned short getSize()
+    {
+        return 4;
+    }
+}KBD_PARAM;
 
-    ptr = strtok(buff, delimiter);
-    if(first_delimiter_pos == 0 && end_delimiter_pos == 0) {
-        if(ptr != NULL)
-        {
-            // Code title
-            len = strlen(ptr);
-            for(i = 0; i < len; i++) {
-                *str_buff++ = *ptr++;
-            }
-            *str_buff = '\0';
-        }
-    }else {
-        // Code suffix
-        for(i = 1; i <= end_delimiter_pos; i++) {
-            ptr = strtok(NULL, delimiter);
-            if(ptr != NULL) {
-                if(first_delimiter_pos <= i) {
-                    len = strlen(ptr);
-                    *str_buff++ = *delimiter;
-                    for(j=0; j < len; j++) {
-                        *str_buff++ = *ptr++;
-                    }
-                }
-            }
-        }
-        *str_buff = '\0';
+template<typename T>
+void cutValueByLimit(T& val, const LIMIT_INFO& limit)
+{
+    if(typeid(T) == typeid(float))
+    {
+        val = (val > (T)limit.float_limit.max) ? (T)limit.float_limit.max : (val);
+        val = (val < (T)limit.float_limit.min) ? (T)limit.float_limit.min : (val);        
+    }
+    else if(typeid(T) == typeid(int))
+    {
+        val = (val > (T)limit.int_limit.max) ? (T)limit.int_limit.max : (val);
+        val = (val < (T)limit.int_limit.min) ? (T)limit.int_limit.min : (val);
+    }
+    else
+    {
+        val = (val > (T)limit.uint_limit.max) ? (T)limit.uint_limit.max : (val);
+        val = (val < (T)limit.uint_limit.min) ? (T)limit.uint_limit.min : (val);
     }
 }
+
+template<typename T>
+void cutValueByMaxVal(T& val, T max_val)
+{
+    if(val > max_val)
+    {
+        val = max_val;
+    }
+    else if(val < (-max_val))
+    {
+        val = -max_val;
+    }
+}
+#define NKB_INTEGER_BITS_MAX 9
+
+template<typename T, unsigned short digits_max>
+T getMaxIntegerValByDigits(unsigned short digits)
+{
+    T ret_val = T();
+
+    if(digits > digits_max)
+    {
+        if(typeid(T) == typeid(unsigned int))
+        {
+            ret_val = static_cast<T>(UINT_MAX);
+        }
+        else
+        {
+            ret_val = static_cast<T>(INT_MAX);
+        }
+        printf("1\n");
+        printf("ret_val = %d\n", ret_val);
+    }
+    else
+    {
+        for(unsigned int i = 0; i < digits; i++)
+        {
+            ret_val += (9 * pow(10,i));
+            printf("ret_val = %d\n", ret_val);
+        }
+        printf("2\n");
+
+    }
+
+    return ret_val;
+}
+
+LIMIT_INFO  lmt;
+KBD_PARAM   m_info;
 
 int main()
 {
-    //char ms_code_val[SC_MS_CODE_STR_NUM];
-    char ms_code_title_suffix[SC_MS_CODE_STR_NUM];
-    char ms_code_buff[SC_MS_CODE_STR_NUM];
-
-    printf("ms_code_origin = %s\n", str);
-
-    spritMsCodeStr(str, 
-        ms_code_title_suffix, 
-        SC_MS_CODE_DELIMITER_SLASH, 
-        SC_MODEL_FIRST_DELIMITER, 
-        SC_MODEL_END_DELIMITER);
-    printf("ms_code_title_suffix = %s\n", ms_code_title_suffix);
-
-    spritMsCodeStr(ms_code_title_suffix, 
-        ms_code_buff, 
-        SC_MS_CODE_DELIMITER_DASH, 
-        SC_MODEL_FIRST_DELIMITER, 
-        SC_MODEL_END_DELIMITER);
-    printf("ms_code_title = %s\n", ms_code_buff);
-
-    spritMsCodeStr(ms_code_title_suffix, 
-        ms_code_buff, 
-        SC_MS_CODE_DELIMITER_DASH, 
-        SC_SUFFIX1_FIRST_DELIMITER, 
-        SC_SUFFIX1_END_DELIMITER);
-    printf("ms_code_suffix1 = %s\n", ms_code_buff);
-
-    spritMsCodeStr(ms_code_title_suffix, 
-        ms_code_buff, 
-        SC_MS_CODE_DELIMITER_DASH, 
-        SC_SUFFIX2_FIRST_DELIMITER, 
-        SC_SUFFIX2_END_DELIMITER);
-    printf("ms_code_suffix2 = %s\n", ms_code_buff);
-
-    spritMsCodeStr(ms_code_title_suffix, 
-        ms_code_buff, 
-        SC_MS_CODE_DELIMITER_DASH, 
-        SC_SUFFIX3_FIRST_DELIMITER, 
-        SC_SUFFIX3_END_DELIMITER);
-    printf("ms_code_suffix3 = %s\n", ms_code_buff);
-
-    spritMsCodeStr(str, 
-        ms_code_buff, 
-        SC_MS_CODE_DELIMITER_SLASH, 
-        SC_OPTIONS_FIRST_DELIMITER, 
-        SC_OPTIONS_END_DELIMITER);
-    printf("ms_code_options = %s\n", ms_code_buff);
+    lmt.int_limit.max = 10;
+    lmt.int_limit.min = -10;
+    //m_info.limit.float_limit.min = FLT_MIN;
+    int            uint_a = 20;
+    printf("int orig: %d\n", uint_a);
+    cutValueByLimit(uint_a, lmt);    
+    //cutValueByMaxVal<unsigned int>(uint_a, int_b);
+    printf("int cutted: %d\n", uint_a);
+    printf("============================\n");
+    uint_a = getMaxIntegerValByDigits<unsigned int, NKB_INTEGER_BITS_MAX>(8);
+    printf("uint max: %d\n", uint_a);
+    printf("============================\n");
+    printf("============================\n");
+    uint_a = getMaxIntegerValByDigits<unsigned int, NKB_INTEGER_BITS_MAX>(11);
+    printf("uint max: %u\n", uint_a);
+    printf("============================\n");
+    uint_a = getMaxIntegerValByDigits<int, NKB_INTEGER_BITS_MAX>(8);
+    printf("int max: %d\n", uint_a);
+    printf("============================\n");
+    uint_a = getMaxIntegerValByDigits<int, NKB_INTEGER_BITS_MAX>(11);
+    printf("int max: %d\n", uint_a);
+    printf("============================\n"); 
+    long total = 0;
+    int x = 10, y = 0;
+    for(int i = 0; i< 9; i++)
+    {
+        total += 9 * pow(x,i); /*调用pow函数*/
+        printf("%ld\n",total);
+    }           
 }
-#endif
