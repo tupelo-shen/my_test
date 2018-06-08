2267ed31a270a2de8a1c842fee386149e7c87537
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2018-06-08 16:04:52 +0800

    2018/06/08

--- main.cpp
+++ main.cpp
@@ -316,3 +316,18 @@ int main()
     int_b = getFloatIntegerDigits(-12.34);
     printf("int_b = %d;\n", int_b);                           
 }
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+static int hello_init(void)
+{
+    printk(KERN_ALERT "Hello, world\n");
+    return 0;
+}
+static void hello_exit(void)
+{
+    printk(KERN_ALERT "Goodbye, cruel world\n");
+}
+module_init(hello_init);
+module_exit(hello_exit);
