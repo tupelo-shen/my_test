fd3a5889b1387828ca8c2748cbbcfdc3bc5a6f88
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2018-10-17 15:43:17 +0800

    2018/10/17-15:43

--- doc/linux/qemu/linux device drivers 3-source codes/lddbus/lddbus.c
+++ doc/linux/qemu/linux device drivers 3-source codes/lddbus/lddbus.c
@@ -14,9 +14,10 @@ MODULE_LICENSE("Dual BSD/GPL");
 static char *Version = "$Revision: 1.9 $";
 
 /*
- * 响应hotplug事件
+ * 响应ldd_uevent事件
  */
-static int ldd_hotplug(struct device *dev, char **envp, int num_envp,
+#if 0
+static int ldd_uevent(struct device *dev, char **envp, int num_envp,
         char *buffer, int buffer_size)
 {
     envp[0] = buffer;
@@ -26,12 +27,24 @@ static int ldd_hotplug(struct device *dev, char **envp, int num_envp,
     envp[1] = NULL;
     return 0;
 }
+#else
+static int ldd_uevent(struct device *dev,struct kobj_uevent_env *env)
+{
+    env->envp[0] = env->buf;
+    if (snprintf(env->buf, env->buflen, "LDDBUS_VERSION=%s",
+                Version) >= env->buflen)
+        return -ENOMEM;
+    env->envp[1] = NULL;
+    return 0;
+}
+#endif
 
 /*
  * 匹配ldd的设备和驱动，仅仅作一个简单的名称检查
  */
 static int ldd_match(struct device *dev, struct device_driver *driver)
 {
+    //return !strncmp(dev->bus_id, driver->name, strlen(driver->name));
     return !strncmp(dev_name(dev), driver->name, strlen(driver->name));
 }
 
@@ -44,9 +57,9 @@ static void ldd_bus_release(struct device *dev)
 }
 
 struct device ldd_bus = {
-    .bus_id   = "ldd0",
-    .release  = ldd_bus_release
-}
+    .init_name = "ldd0",    /*bus_id   = "ldd0",*/
+    .release  = ldd_bus_release,
+};
 
 /*
  * 总线类型
@@ -54,7 +67,7 @@ struct device ldd_bus = {
 struct bus_type ldd_bus_type = {
     .name = "ldd",
     .match = ldd_match,
-    .hotplug  = ldd_hotplug,
+    .uevent = ldd_uevent, /* hotplug  = ldd_hotplug, */
 };
 
 /*
@@ -86,7 +99,8 @@ int register_ldd_device(struct ldd_device *ldddev)
     ldddev->dev.bus = &ldd_bus_type;
     ldddev->dev.parent = &ldd_bus;
     ldddev->dev.release = ldd_dev_release;
-    strncpy(ldddev->dev.bus_id, ldddev->name, BUS_ID_SIZE);
+    strncpy(ldddev->dev.init_name, ldddev->name, strlen(ldddev->name));
+    dev_set_name(&(ldddev->dev), "%s", ldddev->name);
     return device_register(&ldddev->dev);
 }
 EXPORT_SYMBOL(register_ldd_device);
@@ -118,7 +132,7 @@ int register_ldd_driver(struct ldd_driver *driver)
     if (ret)
         return ret;
     driver->version_attr.attr.name = "version";
-    driver->version_attr.attr.owner = driver->module;
+    //driver->version_attr.attr.owner = driver->module;
     driver->version_attr.attr.mode = S_IRUGO;
     driver->version_attr.show = show_version;
     driver->version_attr.store = NULL;
